698e72740f0b3774f7148ba22613b8d4f20dce8e
victoria655 <karonjivictoria@gmail.com>
2025-03-17 11:11:07 +0300

    modified index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+ function receivesAFunction(callback) {
+    callback(); }
+
+ function returnsANamedFunction() {
+  return function namedFunction() {
+console.log("I am a named function!");
+        };
+      }
+
+
+ function returnsAnAnonymousFunction (){
+    return function(){
+    console.log('i am a girl')
+    }
+ }
+    
